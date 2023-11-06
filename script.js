df8223ab5d542c81345bbb46b50ccc1db6815a31
Phelyks <flexxmosh@gmail.com>
2023-11-06 20:48:11 +0300

    update

--- script.js
+++ script.js
@@ -27,16 +27,20 @@ function imgOff() {
 function toggleButtonColors(clickedButton){
     var button1 = document.getElementById('button1');
     var button2 = document.getElementById('button2');
-    
+    var container = document.getElementById('cont');
+
     if (clickedButton === 1) {
         button1.style.backgroundColor = 'green';
         button1.style.color = 'white';
         button2.style.backgroundColor = 'red';
         button2.style.color = 'black';
+        container.style.backgroundColor = 'gray';
     } else if (clickedButton === 2) {
         button1.style.backgroundColor = 'red';
         button1.style.color = 'green';
         button2.style.backgroundColor = 'green';
         button2.style.color = 'white';
+        container.style.backgroundColor = 'black';
+
     }
 }
