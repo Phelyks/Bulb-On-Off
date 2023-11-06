798ca0ecd5abe1c4018c11107a8754a277b60698
Phelyks <flexxmosh@gmail.com>
2023-11-06 15:53:23 +0300

    update

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ function imgOn() {
         img.alt = 'Bulb On';
     }
 
-    
+
 }
 
 function imgOff() {
@@ -23,3 +23,18 @@ function imgOff() {
         img.alt = 'Bulb Off';
     }
 }
+
+
+function toggleButtonColors(clickedButton) {
+            if (clickedButton === 1) {
+                button1.style.backgroundColor = "green";
+                button1.style.color = "white";
+                button2.style.backgroundColor = "red";
+                button2.style.color = "black";
+            } else if (clickedButton === 2) {
+                button1.style.backgroundColor = "red";
+                button1.style.color = "black";
+                button2.style.backgroundColor = "green";
+                button2.style.color = "white";
+            }
+        }
